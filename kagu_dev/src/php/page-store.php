d5aedd7ea8155287e06a3eda9b1525b3c8b230e9
Hidekazu Takahashi <postman.th@gmail.com>
2021-04-09 09:49:17 +0900

    販売店・展示場の親ページ作成

--- kagu_dev/src/php/page-store.php
+++ kagu_dev/src/php/page-store.php
@@ -18,19 +18,31 @@
         <section class="p-section">
           <div class="l-container p-section__content p-alternate">
             <div class="p-alternate__item p-storePageItem">
-              <figure class="p-alternate__itemPrimary c-img p-storePageItem__img"><img src="" alt=""></figure><!-- /.p-alternate__itemPrimary -->
+              <figure class="p-alternate__itemPrimary p-storePageItem__img">
+                <div class="c-img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/store/img_store.jpg" alt="ストアマップ">
+                </div><!-- /.c-img -->
+              </figure><!-- /.p-alternate__itemPrimary -->
               <div class="p-alternate__itemSecondary p-storePageItem__body">
-                <h2 class="p-storePageItem__title">販売店</h2><!-- /.p-storePageItem__heading -->
-                <p class="p-storePageItem__text">KAGUの販売店をご覧いただけます。<br>お客様の「実物を見たい！」を叶えるため、北海道から九州まで、どこにでもKAGUはあります。</p><!-- /.p-storePageItem__text -->
-                <a href="" class="c-btn p-storePageItem__link">販売店一覧へ</a><!-- /.c-btn p-storePageItem__link -->
+                <h2 class="p-storePageItem__title p-alternate__itemTitle">販売店</h2><!-- /.p-storePageItem__heading -->
+                <p class="p-storePageItem__text p-alternate__itemText">KAGUの販売店をご覧いただけます。<br>お客様の「実物を見たい！」を叶えるため、北海道から九州まで、どこにでもKAGUはあります。</p><!-- /.p-storePageItem__text -->
+                <div class="p-alternate__itemFooter">
+                  <a href="<?php echo esc_html(home_url('/shop')); ?>" class="c-btn p-storePageItem__link">販売店一覧へ</a><!-- /.c-btn p-storePageItem__link -->
+                </div><!-- /.p-alternate__itemFooter -->
               </div><!-- /.p-alternate__itemSecondary -->
             </div><!-- /.p-alternate__item -->
             <div class="p-alternate__item p-storePageItem">
-              <figure class="p-alternate__itemPrimary c-img p-storePageItem__img"><img src="" alt=""></figure><!-- /.p-alternate__itemPrimary -->
+              <figure class="p-alternate__itemPrimary p-storePageItem__img">
+                <div class="c-img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/store/img_showroom.jpg" alt="ショールームイメージ">
+                </div><!-- /.c-img -->
+              </figure><!-- /.p-alternate__itemPrimary -->
               <div class="p-alternate__itemSecondary p-storePageItem__body">
-                <h2 class="p-storePageItem__title">展示場</h2><!-- /.p-storePageItem__heading -->
-                <p class="p-storePageItem__text">KAGUの家具は全国のショールームにてご覧いただけます。<br>事前にご予約いただきますと、スタッフがショールームをご案内致します。</p><!-- /.p-storePageItem__text -->
-                <a href="" class="c-btn p-storePageItem__link">展示場一覧へ</a><!-- /.c-btn p-storePageItem__link -->
+                <h2 class="p-storePageItem__title p-alternate__itemTitle">展示場</h2><!-- /.p-storePageItem__heading -->
+                <p class="p-storePageItem__text p-alternate__itemText">KAGUの家具は全国のショールームにてご覧いただけます。<br>事前にご予約いただきますと、スタッフがショールームをご案内致します。</p><!-- /.p-storePageItem__text -->
+                <div class="p-alternate__itemFooter">
+                  <a href="<?php echo esc_html(home_url('/showroom')); ?>" class="c-btn p-storePageItem__link">展示場一覧へ</a><!-- /.c-btn p-storePageItem__link -->
+                </div><!-- /.p-alternate__itemFooter -->
               </div><!-- /.p-alternate__itemSecondary -->
             </div><!-- /.p-alternate__item -->
           </div><!-- /.l-container p-section__content -->
