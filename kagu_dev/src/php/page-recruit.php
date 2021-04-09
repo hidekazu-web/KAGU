d4fd2daa96573a9f46b34a23dd4c82e7c11c056b
Hidekazu Takahashi <postman.th@gmail.com>
2021-04-09 17:43:23 +0900

    募集要項ページ完成

--- kagu_dev/src/php/page-recruit.php
+++ kagu_dev/src/php/page-recruit.php
@@ -16,30 +16,32 @@
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
         <section class="p-section p-recruitSection">
-          <div class="p-section__title">募集要項<span>requirements</span></div><!-- /.p-section__title-->
           <div class="l-container l-container--narrow p-section__content">
-            <div class="p-recruitSection__body">
-              <?php $items = scf::get('recruit_guidelines');
-              if ($items) :
-              ?>
-                <table class="p-recruitSection__table p-recruitTable p-table">
-                  <?php foreach ($items as $item) : ?>
-                    <tr>
-                      <th class="p-table__head"><?php echo esc_html($item['item']); ?></th>
-                      <td class="p-table__content">
-                        <div class="p-table__text"><?php echo esc_html($item['text']); ?></div><!-- /.p-table__text -->
-                        <?php if ($item['sub_text']) : ?>
-                          <div class="p-table__subText"><?php echo esc_html($item['sub_text']); ?></div><!-- /.p-table__subText -->
-                        <?php endif; ?>
-                      </td>
-                    </tr>
-                  <?php endforeach; ?>
-                </table><!-- /.p-recruitSection__table p-recruitTable p-table -->
-              <?php endif; ?>
-            </div><!-- /.p-recruitSection__body -->
-            <div class="p-recruitSection__footer">
-              <a href="<?php echo esc_html(home_url('/contact')); ?>" class="c-btn">応募する</a><!-- /.c-btn -->
-            </div><!-- /.p-recruitSection__footer -->
+          <div class="p-section__title">募集要項<span>requirements</span></div><!-- /.p-section__title-->
+            <div class="p-section__main">
+              <div class="p-recruitSection__body">
+                <?php $items = scf::get('recruit_guidelines');
+                if ($items) :
+                ?>
+                  <dl class="p-recruitSection__table p-defList">
+                    <?php foreach ($items as $item) : ?>
+                      <div class="p-defList__row">
+                        <dt class="p-defList__head"><?php echo esc_html($item['item']); ?></dt>
+                        <dd class="p-defList__content">
+                          <div class="p-defList__text"><?php echo esc_html($item['text']); ?></div><!-- /.p-defList__text -->
+                          <?php if ($item['sub_text']) : ?>
+                            <div class="p-defList__subText"><?php echo esc_html($item['sub_text']); ?></div><!-- /.p-defList__subText -->
+                          <?php endif; ?>
+                        </dd>
+                      </div>
+                    <?php endforeach; ?>
+                  </dl><!-- /.p-recruitSection__table p-defList -->
+                <?php endif; ?>
+              </div><!-- /.p-recruitSection__body -->
+              <div class="p-recruitSection__footer">
+                <a href="<?php echo esc_html(home_url('/contact')); ?>" class="c-btn">応募する</a><!-- /.c-btn -->
+              </div><!-- /.p-recruitSection__footer -->
+            </div><!-- /.p-section__main -->
           </div><!-- /.l-container l-container--narrow -->
         </section><!-- /.p-section p-recruitSection -->
         <?php the_content(); ?>
