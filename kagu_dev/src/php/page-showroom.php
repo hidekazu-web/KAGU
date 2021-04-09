68310c5d638137723e30e1f23a677adb8b11858d
Hidekazu Takahashi <postman.th@gmail.com>
2021-04-09 16:07:57 +0900

    展示場一覧ページ完成

--- kagu_dev/src/php/page-showroom.php
+++ kagu_dev/src/php/page-showroom.php
@@ -51,19 +51,21 @@
                   <ul class="p-storeList">
                     <?php foreach ($shop_list as $shop) : ?>
                       <li class="p-storeList__item p-storeItem">
+                        <figure class="p-storeItem__img c-img"><img src="<?php echo wp_get_attachment_image_src($shop['image'], 'large')[0] ?>" alt=""></figure><!-- /.p-storeItem__img c-img -->
                         <div class="p-storeItem__body">
-                          <figure class="p-storeItem__img c-img"><img src="<?php echo wp_get_attachment_image_src($shop['image'], 'medium')[0] ?>" alt=""></figure><!-- /.p-storeItem__img c-img -->
                           <div class="p-storeItem__title"><?php echo esc_html($shop['title']); ?></div><!-- /.p-storeItem__title -->
                           <address class="p-storeItem__address">
-                            <div class="p-storeItem__post"><?php echo esc_html($shop['postalCode']); ?></div><!-- /.p-storeItem__post -->
-                            <div class="p-storeItem__add"><?php echo esc_html($shop['address']); ?></div><!-- /.p-storeItem__add -->
+                            <span class="p-storeItem__post">&#12306;<?php echo esc_html($shop['postalCode']); ?></span><!-- /.p-storeItem__post -->
+                            <span class="p-storeItem__add"><?php echo esc_html($shop['address']); ?></span><!-- /.p-storeItem__add -->
                           </address><!-- /.p-storeItem__address -->
-                          <div class="p-storeItem__tel"><?php echo esc_html($shop['tel']); ?></div><!-- /.p-storeItem__tel -->
-                          <div class="p-storeItem__fax"><?php echo esc_html($shop['fax']); ?></div><!-- /.p-storeItem__fax -->
-                          <div class="p-storeItem__footer">
-                            <a href="<?php echo esc_html($shop['map_link']); ?>" target="_blank" rel="noopener noreferrer" class="c-btn c-btn--outerLink">Google Mapで見る</a><!-- /.c-btn c-btn--outerLink -->
-                          </div><!-- /.p-storeItem__footer -->
+                          <div class="p-storeItem__contact">
+                            <span class="p-storeItem__tel">TEL:<?php echo esc_html($shop['tel']); ?></span><!-- /.p-storeItem__tel -->
+                            <span class="p-storeItem__fax">FAX:<?php echo esc_html($shop['fax']); ?></span><!-- /.p-storeItem__fax -->
+                          </div><!-- /.p-storeItem__contact -->
                         </div><!-- /.p-storeItem__body -->
+                        <div class="p-storeItem__footer">
+                          <a href="<?php echo esc_html($shop['map_link']); ?>" target="_blank" rel="noopener noreferrer" class="c-btn c-btn--outerLink">Google Mapで見る</a><!-- /.c-btn c-btn--outerLink -->
+                        </div><!-- /.p-storeItem__footer -->
                       </li><!-- /.p-storeList__item -->
                     <?php endforeach; ?>
                   </ul><!-- /.p-storeList -->
